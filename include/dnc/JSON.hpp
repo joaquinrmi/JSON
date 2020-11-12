83fdbd7a57a6df8846c03b2e5850fb5de79fb622
joaquinrmi <joaquinruaimi@gmail.com>
2020-11-12 15:06:42 -0300

    added UTF8Tokenizator

--- include/dnc/JSON.hpp
+++ include/dnc/JSON.hpp
@@ -1,3 +1,9 @@
+/*
+	JSON
+	Joaquín Ruaimi
+	https://github.com/joaquinrmi/JSON
+*/
+
 #pragma once
 
 #include <exception>
@@ -1028,6 +1034,248 @@ namespace dnc
 	}
 
 	/*
+		TextStatus
+	*/
+	class TextStatus
+	{
+	public:
+		TextStatus();
+		TextStatus(const std::string& message);
+
+		bool ok() const noexcept;
+
+	private:
+		bool status;
+		std::string message;
+	};
+
+	TextStatus::TextStatus() :
+		status(true)
+	{}
+	
+	TextStatus::TextStatus(const std::string& message) :
+		status(false),
+		message(message)
+	{}
+
+	bool TextStatus::ok() const noexcept
+	{
+		return status;
+	}
+
+	/*
+		class TextToken
+	*/
+	class TextToken
+	{
+	public:
+		enum Type
+		{
+			SPACE,
+			SYMBOL,
+			WORD,
+			NUMBER
+		};
+
+		TextToken(Type type, const std::string& value);
+		TextToken(Type type, std::string&& value);
+
+		Type type;
+		std::string value;
+	};
+
+	TextToken::TextToken(Type type, const std::string& value) :
+		type(type),
+		value(value)
+	{}
+
+	TextToken::TextToken(Type type, std::string&& value) :
+		type(type),
+		value(std::move(value))
+	{}
+
+	enum CharType
+	{
+		C_BLANK,
+		C_SYMBOL,
+		C_NUMBER,
+		C_LETTER
+	};
+
+	/*
+		class UTF8Tokenizator
+	*/
+	class UTF8Tokenizator
+	{
+	public:
+		static TextStatus getToken(const std::string& text, uint32_t pos, TextToken& target);
+
+	private:
+		static const std::vector<CharType> CHAR_TYPES;
+		static const uint8_t NUMBER_POINT;
+
+		UTF8Tokenizator();
+	};
+
+	const std::vector<CharType> UTF8Tokenizator::CHAR_TYPES({
+		C_BLANK, C_BLANK, C_BLANK, C_BLANK, C_BLANK, C_BLANK, C_BLANK, C_BLANK,
+		C_BLANK, C_BLANK, C_BLANK, C_BLANK, C_BLANK, C_BLANK, C_BLANK, C_BLANK,
+		C_BLANK, C_BLANK, C_BLANK, C_BLANK, C_BLANK, C_BLANK, C_BLANK, C_BLANK,
+		C_BLANK, C_BLANK, C_BLANK, C_BLANK, C_BLANK, C_BLANK, C_BLANK, C_BLANK,
+		C_BLANK, C_SYMBOL, C_SYMBOL, C_SYMBOL, C_SYMBOL, C_SYMBOL, C_SYMBOL, C_SYMBOL,
+		C_SYMBOL, C_SYMBOL, C_SYMBOL, C_SYMBOL, C_SYMBOL, C_SYMBOL, C_SYMBOL, C_SYMBOL,
+		C_NUMBER, C_NUMBER, C_NUMBER, C_NUMBER, C_NUMBER, C_NUMBER, C_NUMBER, C_NUMBER,
+		C_NUMBER, C_NUMBER, C_SYMBOL, C_SYMBOL, C_SYMBOL, C_SYMBOL, C_SYMBOL, C_SYMBOL,
+		C_SYMBOL, C_LETTER, C_LETTER, C_LETTER, C_LETTER, C_LETTER, C_LETTER, C_LETTER,
+		C_LETTER, C_LETTER, C_LETTER, C_LETTER, C_LETTER, C_LETTER, C_LETTER, C_LETTER,
+		C_LETTER, C_LETTER, C_LETTER, C_LETTER, C_LETTER, C_LETTER, C_LETTER, C_LETTER,
+		C_LETTER, C_LETTER, C_LETTER, C_SYMBOL, C_SYMBOL, C_SYMBOL, C_SYMBOL, C_SYMBOL,
+		C_SYMBOL, C_LETTER, C_LETTER, C_LETTER, C_LETTER, C_LETTER, C_LETTER, C_LETTER,
+		C_LETTER, C_LETTER, C_LETTER, C_LETTER, C_LETTER, C_LETTER, C_LETTER, C_LETTER,
+		C_LETTER, C_LETTER, C_LETTER, C_LETTER, C_LETTER, C_LETTER, C_LETTER, C_LETTER,
+		C_LETTER, C_LETTER, C_LETTER, C_SYMBOL, C_SYMBOL, C_SYMBOL, C_SYMBOL, C_BLANK
+	});
+
+	const uint8_t UTF8Tokenizator::NUMBER_POINT = '.';
+
+	UTF8Tokenizator::UTF8Tokenizator()
+	{}
+
+	TextStatus UTF8Tokenizator::getToken(const std::string& text, uint32_t pos, TextToken& target)
+	{
+		int32_t char_count = 0;
+		if(!UTF8Analyzer::countNextChar(text, char_count, pos))
+		{
+			return TextStatus("");
+		}
+
+		if(char_count == 1)
+		{
+			uint8_t c = text[pos];
+			auto char_type = CHAR_TYPES[c];
+
+			switch(char_type)
+			{
+			case C_BLANK:
+			{
+				target.type = TextToken::SPACE;
+				target.value = text.substr(pos, char_count);
+
+				auto pos2 = pos + 1;
+				while(true)
+				{
+					int32_t char_count_2 = 0;
+					if(!UTF8Analyzer::countNextChar(text, char_count_2, pos2))
+					{
+						return TextStatus();
+					}
+
+					if(char_count_2 > 1)
+					{
+						return TextStatus();
+					}
+
+					uint8_t c2 = text[pos2];
+					if(CHAR_TYPES[c2] != C_BLANK)
+					{
+						return TextStatus();
+					}
+
+					target.value += text.substr(pos2, char_count_2);
+					++pos2;
+				}
+
+				break;
+			}
+
+			case C_SYMBOL:
+			{
+				target.type = TextToken::SYMBOL;
+				target.value = text.substr(pos, char_count);
+				return TextStatus();
+			}
+
+			case C_NUMBER:
+			{
+				target.type = TextToken::NUMBER;
+				target.value = text.substr(pos, char_count);
+
+				bool added_point = false;
+
+				auto pos2 = pos + 1;
+				while(true)
+				{
+					int32_t char_count_2 = 0;
+					if(!UTF8Analyzer::countNextChar(text, char_count_2, pos2))
+					{
+						return TextStatus();
+					}
+
+					if(char_count_2 > 1)
+					{
+						return TextStatus();
+					}
+
+					uint8_t c2 = text[pos2];
+					if(CHAR_TYPES[c2] != C_NUMBER && CHAR_TYPES[c2] != C_SYMBOL)
+					{
+						return TextStatus();
+					}
+
+					if(CHAR_TYPES[c2] == C_SYMBOL)
+					{
+						if(c2 == NUMBER_POINT && !added_point)
+						{
+							added_point = true;
+						}
+						else return TextStatus();
+					}
+
+					target.value += text.substr(pos2, char_count_2);
+					++pos2;
+				}
+
+				break;
+			}
+
+			case C_LETTER:
+				target.type = TextToken::WORD;
+				target.value = text.substr(pos, char_count);
+
+				auto pos2 = pos + 1;
+				while(true)
+				{
+					int32_t char_count_2 = 0;
+					if(!UTF8Analyzer::countNextChar(text, char_count_2, pos2))
+					{
+						return TextStatus();
+					}
+
+					if(char_count_2 > 1)
+					{
+						return TextStatus();
+					}
+
+					uint8_t c2 = text[pos2];
+					if(CHAR_TYPES[c2] != C_LETTER)
+					{
+						return TextStatus();
+					}
+
+					target.value += text.substr(pos2, char_count_2);
+					++pos2;
+				}
+
+				break;
+			}
+		}
+
+		target.type = TextToken::SYMBOL;
+		target.value = text.substr(pos, char_count);
+		return TextStatus();
+	}
+
+	/*
 		class JSONParseStatus
 	*/
 	class JSONParseStatus
@@ -1078,7 +1326,7 @@ namespace dnc
 	};
 
 	UnexpectedToken::UnexpectedToken(const std::string& token, int row, int col) :
-		JSONParseStatus("unexpected token '" + token + "' at position " + StringUtils::toString(row) + ":" + StringUtils::toString(col))
+		JSONParseStatus(std::move("unexpected token '" + token + "' at position " + StringUtils::toString(row) + ":" + StringUtils::toString(col)))
 	{}
 
 	UnexpectedToken::~UnexpectedToken()
@@ -1095,7 +1343,7 @@ namespace dnc
 	};
 
 	CannotOpenFile::CannotOpenFile(const std::string& filename) :
-		JSONParseStatus("cannot open file " + filename)
+		JSONParseStatus(std::move("cannot open file " + filename))
 	{}
 
 	CannotOpenFile::~CannotOpenFile()
@@ -1112,32 +1360,47 @@ namespace dnc
 		static JSONParseStatus parseFromFile(const std::string& filename, JSON& target);
 
 	private:
-		static const char BRACKET_OPEN;
-		static const char BRACKET_CLOSE;
-		static const char SQUARE_BRACKET_OPEN;
-		static const char SQUARE_BRACKET_CLOSE;
-		static const char QUOTATION_MARK;
-		static const char COMMA;
-		static const char COLON;
-		static const char SEMICOLON;
+		static const uint8_t BRACKET_OPEN;
+		static const uint8_t BRACKET_CLOSE;
+		static const uint8_t SQUARE_BRACKET_OPEN;
+		static const uint8_t SQUARE_BRACKET_CLOSE;
+		static const uint8_t QUOTATION_MARK;
+		static const uint8_t COMMA;
+		static const uint8_t COLON;
+		static const uint8_t SEMICOLON;
 
 		JSONParser();
+
+		static JSONParseStatus getJSONElement(const std::string& text, uint32_t& pos, JSON& target);
 	};
 
-	const char JSONParser::BRACKET_OPEN = '{';
-	const char JSONParser::BRACKET_CLOSE = '}';
-	const char JSONParser::SQUARE_BRACKET_OPEN = '[';
-	const char JSONParser::SQUARE_BRACKET_CLOSE = ']';
-	const char JSONParser::QUOTATION_MARK = '\"';
-	const char JSONParser::COMMA = ',';
-	const char JSONParser::COLON = ':';
-	const char JSONParser::SEMICOLON = ';';
+	const uint8_t JSONParser::BRACKET_OPEN = '{';
+	const uint8_t JSONParser::BRACKET_CLOSE = '}';
+	const uint8_t JSONParser::SQUARE_BRACKET_OPEN = '[';
+	const uint8_t JSONParser::SQUARE_BRACKET_CLOSE = ']';
+	const uint8_t JSONParser::QUOTATION_MARK = '\"';
+	const uint8_t JSONParser::COMMA = ',';
+	const uint8_t JSONParser::COLON = ':';
+	const uint8_t JSONParser::SEMICOLON = ';';
 
 	JSONParser::JSONParser()
 	{}
 
 	JSONParseStatus JSONParser::parse(const std::string& text, JSON& target)
-	{}
+	{
+		JSON temp;
+
+		uint32_t position = 0;
+		auto status = getJSONElement(text, position, temp);
+		if(!status.ok())
+		{
+			return status;
+		}
+
+		target = std::move(temp);
+
+		return JSONParseStatus();
+	}
 
 	JSONParseStatus JSONParser::parse(const std::string& text, uint32_t pos, JSON& target)
 	{}
@@ -1150,6 +1413,9 @@ namespace dnc
 			return CannotOpenFile(filename);
 		}
 	}
+
+	JSONParseStatus JSONParser::getJSONElement(const std::string& text, uint32_t& pos, JSON& target)
+	{}
 
 	/*
 		class JSON
@@ -1647,7 +1913,7 @@ namespace dnc
 	template<typename T>
 	T& JSON::get()
 	{
-		throw JSONBadType("El tipo solicitado es un tipo fundamental de JSON");
+		throw JSONBadType("El tipo solicitado no es un tipo fundamental de JSON");
 	}
 
 	template<>
